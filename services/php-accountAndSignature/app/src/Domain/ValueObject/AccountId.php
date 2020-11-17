be0343129be3779cf9d0fe34ba07da703530504e
jmartinixta <jmartinixta@gmail.com>
2020-11-17 16:02:05 -0600

    valueObjects, entity and routes for accountUpdater

---
+++ services/php-accountAndSignature/app/src/Domain/ValueObject/AccountId.php
@@ -0,0 +1,4 @@
+<?php
+namespace App\Domain\ValueObject;
+
+class AccountId extends StringValueObject { }
---
+++ services/php-accountAndSignature/app/src/Domain/ValueObject/Email.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Domain\ValueObject;
+
+class Email extends StringValueObject
+{
+    private string $email;
+
+    public function __construct(string $email)
+    {
+        $this->email = $email;
+        $this->emailSyntax($email);
+    }
+
+    private function emailSyntax(string $email): void
+    {
+        $valid_word = "/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/";
+        if (preg_match($valid_word, $email)) return;
+        throw new \Exception($email . ' is not valid as email');
+    }
+
+    public function toString(): string
+    {
+        return $this->email;
+    }
+}
