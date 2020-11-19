538136129e78f844f94dc8a7c43d085e838bd38d
Angel Ricardo <angelricardo.dev@gmail.com>
2020-11-18 22:27:34 -0600

    Feature: User Credentials
    Api routes for register and login

--- services/user-credentials/src/infraestructure/server/api-routes.ts
+++ services/user-credentials/src/infraestructure/server/api-routes.ts
@@ -1,5 +1,36 @@
 import { Router } from 'express'
+import { MySqlUserRepository } from '../database/MySqlUserRepository'
+import { UserRegisterController } from '../controller/user-register/UserRegisterController'
+import { CredentialsValidatorController } from '../controller/credentials-validator/CredentialsValidatorController'
 export const router = Router()
+
+const user_repository = new MySqlUserRepository()
+
+router.post('/register', async (request, response) => {
+  const controller = new UserRegisterController(user_repository)
+
+  const controllerResponse = await controller.handler({
+    email: request.body.email,
+    firstName: request.body.firstName,
+    lastName: request.body.lastName,
+    password: request.body.password,
+    signature: request.body.signature,
+    enterprise_account: request.body.enterprise_account
+  })
+
+  response.json(controllerResponse)
+})
+
+router.post('/login', async (request, response) => {
+  const controller = new CredentialsValidatorController(user_repository)
+
+  const controllerResponse = await controller.handler({
+    email: request.body.email,
+    password: request.body.password
+  })
+
+  response.json(controllerResponse)
+})
 
 router.all('*', (request, response) => {
   response.status(404).send()
