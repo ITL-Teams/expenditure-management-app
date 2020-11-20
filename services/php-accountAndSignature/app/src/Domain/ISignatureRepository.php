e60bbba82d9b823855d0156e88833f8d4c6ba41c
Angel Ricardo <angelricardo.dev@gmail.com>
2020-11-20 00:35:09 -0600

    Deleting old Account and Signature directory

--- services/php-accountAndSignature/app/src/Domain/ISignatureRepository.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace App\Domain;
-
-use App\Domain\Entity\Signature;
-use App\Domain\ValueObject\SignatureId;
-
-interface ISignatureRepository {
-  public function create(Signature $signature): void;
-}
--- services/php-accountAndSignature/app/src/index.php
+++
@@ -1,4 +0,0 @@
-<?php
-declare(strict_types=1);
-require '../app/vendor/autoload.php';
-require_once '../app/src/infraestructure/server/app.php';
